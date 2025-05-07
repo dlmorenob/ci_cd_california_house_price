3db136625da41534c5c5a5da1b3c55462d12a6f0
David Leonardo Moreno Bedoya <leonardo.moreno@transmilenio.gov.co>
2025-05-06 20:17:19 -0500

    cambios para ir viendo la evolucion del modelo

--- src/train.py
+++ src/train.py
@@ -1,1 +1,23 @@
-print("entra a train.py")
+import os
+
+
+print(f"--- Entra a Entrenamiento")
+
+print(f"--- Debug: Initial CWD: {os.getcwd()} ---")
+
+# --- Define Paths ---
+# Usar rutas absolutas dentro del workspace del runner
+workspace_dir = os.getcwd() 
+mlruns_dir = os.path.join(workspace_dir, "mlruns")
+tracking_uri = "file://" + os.path.abspath(mlruns_dir)
+# Definir explícitamente la ubicación base deseada para los artefactos
+### DAVID REVISAR y COMPARAR CON EL EJERCICIO QUE SE HIZO ANTES. EL DEL ARTEFACTO PARA SUBIR EL CSV
+artifact_location = "file://" + os.path.abspath(mlruns_dir)
+
+print(f"--- Debug: Workspace Dir: {workspace_dir} ---")
+print(f"--- Debug: MLRuns Dir: {mlruns_dir} ---")
+print(f"--- Debug: Tracking URI: {tracking_uri} ---")
+print(f"--- Debug: Desired Artifact Location Base: {artifact_location} ---")
+
+
+
